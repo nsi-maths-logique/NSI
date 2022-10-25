abbf3a18ca300a9c8b34ec3b6c8cc7080baf8d10
nsi-maths-logique <107237256+nsi-maths-logique@users.noreply.github.com>
2022-10-25 12:18:42 +0200

    Update couples.py

--- couples.py
+++ couples.py
@@ -4,39 +4,51 @@ def cons(e,l):
 def tete(l):
   if l == ():
     return ()
-  else:
-    return l[0]
+  return l[0]
     
 def queue(l):
   if l == ():
     return ()
-  else:
-    return l[1]
+  return l[1]
     
 def elem(n, l):
+  assert n <= longueur(l), 'Le rang est superieur a la longueur de la liste'
   if n == 0:
     return tete(l)
-  else:
-    return elem(n-1, queue(l))    
+  return elem(n-1, queue(l))    
     
 def concatene(l1, l2):
   if l1 == ():
     return l2
-  else:
-    return cons(tete(l1), concatene(queue(l1), l2))
+  return cons(tete(l1), concatene(queue(l1), l2))
   
 def longueur(l):
   if l == ():
     return 0
-  else:
-    return 1 + longueur(queue(l))
+  return 1 + longueur(queue(l))
+
+def nb_occ(e, l):
+  if l == '':
+    return 0
+  if tete(l) == e:
+    return 1 + nb_occ(e, queue(l))
+  return nb_occ(e, queue(l))
+
+def liste_vers_tuple(liste):
+  if liste == []:
+    return ()
+  return cons(liste[0], liste_vers_tuple(liste[1:]))
+
+def tuple_vers_liste(l):
+  if l == ():
+    return []
+  return [tete(l)] + tuple_vers_liste(queue(l))
 
 def inverser(l):
-    if l == ():
-        return ()
-    else:
-        elem = cons(tete(l),())
-        return concatene(inverser(queue(l)), elem)
+  if l == ():
+    return ()
+  elem = cons(tete(l),())
+  return concatene(inverser(queue(l)), elem)
 
 def coupe(l):
   m = longueur(l) // 2
@@ -46,9 +58,8 @@ def coupe_liste(l, m, l1):
   if m == 1:
     elem = cons(tete(l),())
     return concatene(l1,elem), queue(l)
-  else:
-    elem = cons(tete(l),())
-    return coupe_liste(queue(l), m-1, concatene(l1,elem))
+  elem = cons(tete(l),())
+  return coupe_liste(queue(l), m-1, concatene(l1,elem))
   
 def fusion(l):
   if longueur(l) == 1:
@@ -60,47 +71,28 @@ def tri(l1, l2):
   out = ()
   if l1 == () or l2 == ():
     return concatene(concatene(out,l1),l2)
-  else:
-    if tete(l1) <= tete(l2):
-      elem = cons(tete(l1),())
-      return concatene(concatene(out, elem), tri(queue(l1), l2))
-    else:
-      elem = cons(tete(l2),())
-      return concatene(concatene(out, elem), tri(l1,queue(l2)))
-      
-def show(l):
-  out = []
-  while l != ():
-    out.append(str(tete(l)))
-    l = queue(l)
-  return '[' + (',').join(out) + ']'
-    
+  if tete(l1) <= tete(l2):
+    elem = cons(tete(l1),())
+    return concatene(concatene(out, elem), tri(queue(l1), l2))
+  elem = cons(tete(l2),())
+  return concatene(concatene(out, elem), tri(l1,queue(l2)))
+          
 def position(e, l):
   n = longueur(l) // 2
   return position_rec(e, l, n)
+
 def position_rec(e, l, m):
   n = longueur(l) // 2
   if longueur(l) == 1:
     if elem(n,l) == e:
       return m
-    else:
-      return -1
+    return -1
   elif elem(n-1,l) == e:
     return m - 1
   elif e < elem(n-1,l):
     l1, l2 = coupe(l)
     n = longueur(l1) // 2
     return position_rec(e, l1, m-n)
-  else:
-    l1, l2 = coupe(l)
-    n = longueur(l2) // 2
-    return position_rec(e, l2, m+n)
-l1 = cons(9,cons(8,cons(7, cons(6,()))))
-l2 = cons(5,cons(4,cons(3, cons(2,()))))
-l = concatene(l1, l2)
-print('l = ', l)
-print('show(l) = ', show(l))
-print('show(fusion(l)) = ', show(fusion(l)))
-print('position(9, fusion(l)) = ', position(9, fusion(l)))
-print('position(11, fusion(l)) = ', position(11, fusion(l)))
-print('show(inverser(fusion(l))) = ', show(inverser(fusion(l))))
+  l1, l2 = coupe(l)
+  n = longueur(l2) // 2
+  return position_rec(e, l2, m+n)
